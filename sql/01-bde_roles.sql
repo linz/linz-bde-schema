e95a7be3736836ecb4d9089b390cba47f1b82de7
Sandro Santilli <strk@kbt.io>
2017-12-10 21:30:13 +0100

    Add roles comments
    See #62

--- sql/01-bde_roles.sql
+++ sql/01-bde_roles.sql
@@ -20,18 +20,30 @@ IF NOT EXISTS (SELECT * FROM pg_roles where rolname = 'bde_dba') THEN
         NOSUPERUSER INHERIT CREATEDB CREATEROLE;
     ALTER ROLE bde_dba SET search_path=bde, bde_control, public;
 END IF;
+COMMENT ON ROLE bde_dba IS $COMMENT$
+'Owns all objects in bde schema. Has rights to manage all of them.'
+$COMMENT$;
 
 IF NOT EXISTS (SELECT * FROM pg_roles where rolname = 'bde_admin') THEN
     CREATE ROLE bde_admin
         NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;
     ALTER ROLE bde_admin SET search_path=bde, public;
 END IF;
+COMMENT ON ROLE bde_admin IS $COMMENT$
+Everything that user can do plus the ability to edit data in the tables.
+$COMMENT$;
 
 IF NOT EXISTS (SELECT * FROM pg_roles where rolname = 'bde_user') THEN
     CREATE ROLE bde_user
         NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;
     ALTER ROLE bde_user SET search_path=bde, public;
 END IF;
+COMMENT ON ROLE bde_user IS $COMMENT$
+Has rights to execute and read anything that bde_dba creates.
+
+Used by users logging in to run analytics queries or for remote users
+to get access to data (e.g LDS import or QGIS desktop users)
+$COMMENT$;
 
 END;
 $ROLES$;
