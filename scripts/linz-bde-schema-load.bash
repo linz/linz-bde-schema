44f332a432b6e440eee7c4ef68d340bce67f44c1
Mitchell Paff <mpaff@linz.govt.nz>
2022-02-01 09:26:44 +1300

    refactor: add safety pragmas

--- scripts/linz-bde-schema-load.bash
+++ scripts/linz-bde-schema-load.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -o errexit -o noclobber -o nounset -o pipefail
+shopt -s failglob inherit_errexit
+
 db_name=
 export SKIP_INDEXES=no
 export ADD_REVISIONS=no
--- test/test-upgrades.bash
+++ test/test-upgrades.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -o errexit -o noclobber -o nounset -o pipefail
+shopt -s failglob inherit_errexit
+
 upgradeable_versions="
     1.9.0
     1.8.0
