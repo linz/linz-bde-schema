d1cf95b286aba5389031102a48496b446fd3eefe
Mitchell Paff <mpaff@linz.govt.nz>
2022-01-31 16:57:56 +1300

    refactor: remove unnecessary semicolon

--- scripts/linz-bde-schema-load.bash
+++ scripts/linz-bde-schema-load.bash
@@ -8,7 +8,8 @@ export READ_ONLY=no
 export PSQL=psql
 export SCRIPTSDIR=/usr/share/linz-bde-schema/sql/
 
-if test "$1" = "--version"; then
+if test "$1" = "--version"
+then
     echo "@@VERSION@@ @@REVISION@@"
     exit 0
 fi
@@ -26,11 +27,13 @@ EOF
 }
 
 
-if test -n "${BDESCHEMA_SQLDIR}"; then
+if test -n "${BDESCHEMA_SQLDIR}"
+then
     SCRIPTSDIR=${BDESCHEMA_SQLDIR}
 fi
 
-if test ! -f "${SCRIPTSDIR}/02-bde_schema.sql"; then
+if test ! -f "${SCRIPTSDIR}/02-bde_schema.sql"
+then
     cat >&2 <<EOF
 Cannot find 02-bde_schema.sql in ${SCRIPTSDIR}
 Please set BDESCHEMA_SQLDIR environment variable
@@ -38,28 +41,39 @@ EOF
     exit 1
 fi
 
-while test -n "$1"; do
-    if test "$1" = "--noindexes"; then
+while test -n "$1"
+do
+    if test "$1" = "--noindexes"
+    then
         SKIP_INDEXES=yes
-        shift; continue
-    elif test "$1" = "--revision"; then
+        shift
+        continue
+    elif test "$1" = "--revision"
+    then
         ADD_REVISIONS=yes
-        shift; continue
-    elif test "$1" = "--readonly"; then
+        shift
+        continue
+    elif test "$1" = "--readonly"
+    then
         READ_ONLY=yes
-        shift; continue
-    elif test "$1" = "--help"; then
+        shift
+        continue
+    elif test "$1" = "--help"
+    then
         usage && exit
-    elif test "$1" = "--noextension"; then
+    elif test "$1" = "--noextension"
+    then
         EXTENSION_MODE=off
-        shift; continue
+        shift
+        continue
     else
         DB_NAME="$1"
         shift
     fi
 done
 
-if test -z "$DB_NAME"; then
+if test -z "$DB_NAME"
+then
     usage >&2
     exit 1
 fi
@@ -85,17 +99,20 @@ dbpatch-loader - fake 2>&1 | grep -q "database.*does not exist" &&
     DBPATCH_SUPPORTS_STDOUT=no ||
     DBPATCH_SUPPORTS_STDOUT=yes
 
-if test "$PGDATABASE" = "-" -a "$DBPATCH_SUPPORTS_STDOUT" != yes; then
+if test "$PGDATABASE" = "-" -a "$DBPATCH_SUPPORTS_STDOUT" != yes
+then
     echo "ERROR: dbpatch-loader does not support stdout mode, cannot proceed." >&2
     echo "HINT: install dbpatch 1.4.0 or higher to fix this." >&2
     exit 1
 fi
 
-if test "${EXTENSION_MODE}" = "off"; then
+if test "${EXTENSION_MODE}" = "off"
+then
     DBPATCH_OPTS=("--no-extension")
 fi
 
-if test "$DBPATCH_SUPPORTS_STDOUT" != yes; then
+if test "$DBPATCH_SUPPORTS_STDOUT" != yes
+then
     echo "WARNING: dbpatch-loader does not support stdout mode, working in non-transactional mode" >&2
     echo "HINT: install dbpatch 1.4.0 or higher to fix this." >&2
     "${DBPATCH_LOADER}" "${DBPATCH_OPTS[@]}" "${PGDATABASE}" _patches || exit 1
@@ -115,17 +132,20 @@ table_version-loader -  2>&1 | grep -q "database.*does not exist" &&
     TABLEVERSION_SUPPORTS_STDOUT=no ||
     TABLEVERSION_SUPPORTS_STDOUT=yes
 
-if test "$PGDATABASE" = "-" -a "$TABLEVERSION_SUPPORTS_STDOUT" != yes; then
+if test "$PGDATABASE" = "-" -a "$TABLEVERSION_SUPPORTS_STDOUT" != yes
+then
     echo "ERROR: table_version-loader does not support stdout mode, cannot proceed" >&2
     echo "HINT: install table_version 1.6.0 or higher to fix this." >&2
     exit 1
 fi
 
-if test "${EXTENSION_MODE}" = "off"; then
+if test "${EXTENSION_MODE}" = "off"
+then
     TABLEVERSION_OPTS=("--no-extension")
 fi
 
-if test "${ADD_REVISIONS}" = "yes" -a "$TABLEVERSION_SUPPORTS_STDOUT" != yes; then
+if test "${ADD_REVISIONS}" = "yes" -a "$TABLEVERSION_SUPPORTS_STDOUT" != yes
+then
     echo "WARNING: table_version-loader does not support stdout mode, working in non-transactional mode" >&2
     echo "HINT: install table_version 1.6.0 or higher to fix this." >&2
     "${TABLEVERSION_LOADER}" "${TABLEVERSION_OPTS[@]}" "${PGDATABASE}" || exit 1
@@ -137,16 +157,19 @@ fi
 
 
 
-if test "$DBPATCH_SUPPORTS_STDOUT" = yes; then
+if test "$DBPATCH_SUPPORTS_STDOUT" = yes
+then
     "${DBPATCH_LOADER}" "${DBPATCH_OPTS[@]}" - _patches || rollback
 fi
 
 # Enable table_version if needed
-if test "${ADD_REVISIONS}" = "yes" -a "$TABLEVERSION_SUPPORTS_STDOUT" = yes; then
+if test "${ADD_REVISIONS}" = "yes" -a "$TABLEVERSION_SUPPORTS_STDOUT" = yes
+then
     "${TABLEVERSION_LOADER}" "${TABLEVERSION_OPTS[@]}" - || rollback
 fi
 
-if test "$PGDATABASE" != "-"; then
+if test "$PGDATABASE" != "-"
+then
     echo 'SET client_min_messages TO WARNING;'
 fi
 
@@ -154,7 +177,8 @@ cat << EOF
 CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;
 EOF
 
-for file in "${SCRIPTSDIR}"/*.sql; do
+for file in "${SCRIPTSDIR}"/*.sql
+do
     if [[ "$SKIP_INDEXES" = 'yes' ]] && [[ "$(basename "$file" .sql)" = '04-bde_schema_index' ]]
     then
         continue
@@ -162,12 +186,14 @@ for file in "${SCRIPTSDIR}"/*.sql; do
     cat "${file}" || rollback
 done
 
-if test "${ADD_REVISIONS}" = "yes"; then
+if test "${ADD_REVISIONS}" = "yes"
+then
     file="${SCRIPTSDIR}/versioning/01-version_tables.sql"
     cat "${file}" || rollback
 fi
 
-if test "${READ_ONLY}" = "yes"; then
+if test "${READ_ONLY}" = "yes"
+then
     cat <<EOF
 REVOKE UPDATE, INSERT, DELETE, TRUNCATE
     ON ALL TABLES IN SCHEMA bde
@@ -180,7 +206,8 @@ echo "COMMIT;"
 } |
 grep -v "^\(BEGIN\|COMMIT\);" |
 ( echo "BEGIN;"; cat; echo "COMMIT;"; ) |
-if test "$PGDATABASE" = "-"; then
+if test "$PGDATABASE" = "-"
+then
     cat
 else
     $PSQL -XtA --set ON_ERROR_STOP=1 -o /dev/null
