5b2f73046934ccd1c9e2e9a3392900c7f7fa2a04
Bill Nelson <bmnelson@linz.govt.nz>
2022-08-02 15:58:14 +1200

    feat: publish revision tables

--- scripts/linz-bde-schema-publish.bash
+++ scripts/linz-bde-schema-publish.bash
@@ -48,15 +48,17 @@ BEGIN
             'Schema bde does not exist, '
             'run linz-bde-schema-load ?';
     END IF;
+
     IF NOT EXISTS ( SELECT 1 FROM pg_catalog.pg_publication
                     WHERE pubname = 'all_bde' )
     THEN
         CREATE PUBLICATION all_bde;
     END IF;
+
     ALTER PUBLICATION all_bde OWNER TO bde_dba;
 
     FOR v_table IN SELECT c.relname from pg_class c, pg_namespace n
-        WHERE n.nspname = 'bde' and c.relnamespace = n.oid
+        WHERE n.nspname = 'bde' AND c.relnamespace = n.oid
         AND c.relkind = 'r' AND c.relname NOT IN (
             SELECT tablename FROM pg_catalog.pg_publication_tables
             WHERE pubname = 'all_bde' AND schemaname = 'bde'
@@ -66,7 +68,36 @@ BEGIN
                        v_table);
     END LOOP;
 
+    IF NOT EXISTS ( SELECT 1 FROM pg_catalog.pg_namespace
+                    WHERE nspname = 'table_version')
+    THEN
+        RAISE EXCEPTION
+            'Schema table_version does not exist, '
+            'have you enabled table versioning?';
+    END IF;
+
+    IF NOT EXISTS ( SELECT 1 FROM pg_catalog.pg_publication
+                    WHERE pubname = 'all_bde_versions' )
+    THEN
+        CREATE PUBLICATION all_bde_versions;
+    END IF;
+
+    ALTER PUBLICATION all_bde_versions OWNER TO bde_dba;
+
+    FOR v_table IN SELECT c.relname from pg_class c, pg_namespace n
+        WHERE n.nspname = 'table_version' AND c.relnamespace = n.oid
+        AND c.relkind = 'r' AND c.relname SIMILAR TO 'bde_(crs|cbe)_%'
+        AND c.relname NOT IN (
+            SELECT tablename FROM pg_catalog.pg_publication_tables
+            WHERE pubname = 'all_bde_versions' AND schemaname = 'table_version'
+        )
+    LOOP
+        EXECUTE format('ALTER PUBLICATION all_bde_versions ADD TABLE table_version.%I',
+                       v_table);
+    END LOOP;
+
     RAISE INFO 'Publication "all_bde" ready';
+    RAISE INFO 'Publication "all_bde_versions" ready';
 
 END;
 \$PUBLICATION\$;
